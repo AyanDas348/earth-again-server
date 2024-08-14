f7b4277c0e0e93c1eae9abd031c45eb14e03e7f7
Ayan Das <dasayan348@gmail.com>
2024-08-14 20:23:15 +0530

    change

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer')
+const mongoose = require('mongoose');
+require('dotenv').config();
 const app = express();
 const port = 3000;
 
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
 
 // Sample data
 const scoresData = [
